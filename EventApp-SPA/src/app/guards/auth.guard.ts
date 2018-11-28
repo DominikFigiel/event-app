adadca2d659d6830a7cdd19984adead0ab5beb02
Dominik Figiel <castro992@gmail.com>
2018-11-29 00:18:28 +0100

    Updated Auth Guard to protect routes based on role

--- EventApp-SPA/src/app/guards/auth.guard.ts
+++ EventApp-SPA/src/app/guards/auth.guard.ts
@@ -1,6 +1,6 @@
 import { AlertifyService } from 'src/app/services/alertify.service';
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -9,12 +9,23 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router,
     private alertify: AlertifyService) {}
-  canActivate(): boolean {
+
+  canActivate(next: ActivatedRouteSnapshot): boolean {
+    const roles = next.firstChild.data['roles'] as Array<string>;
+    if (roles) {
+      const match = this.authService.roleMatch(roles);
+      if (match) {
+        return true;
+      } else {
+        this.router.navigate(['/events']);
+        this.alertify.error('Brak dostępu!');
+      }
+    }
     if (this.authService.loggedIn()) {
       return true;
     }
 
-    this.alertify.error('Nie jesteś zalogowany.');
+    this.alertify.error('Brak dostępu!');
     this.router.navigate(['/home']);
     return false;
   }
